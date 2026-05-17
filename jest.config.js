bd77d5bea7141681bd43d95673fc9b2d718ae09b
Rayhan Atmadja <workspace.rayhanatmadja@gmail.com>
2026-05-17 12:28:05 +0700

    fix(deploy): jest test and double pnpm versions

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,5 @@
-export const preset = 'ts-jest'
-export const testEnvironment = 'node'
+export const preset = 'ts-jest/presets/js-with-ts';
+export const testEnvironment = 'node';
+export const transformIgnorePatterns = [
+    'node_modules/(?!uuid)/'
+];
